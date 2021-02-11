b5636de079478b2b8f5e885d15cbc5d5fe3348cd
Noah Tigner <nzt@cs.uoregon.edu>
2021-02-11 13:31:06 -0800

    Linked List

--- DataStructuresAndAlgorithms/linkedList.py
+++ DataStructuresAndAlgorithms/linkedList.py
@@ -51,6 +51,17 @@ class LinkedList:
                 return
             n = n.next
 
+    def reverse(self):
+        prev = None
+        curr = self.head
+        nxt = None
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        self.head = prev
+
     def print(self):
         n = self.head
         while n:
