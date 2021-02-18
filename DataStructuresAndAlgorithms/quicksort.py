ead2af0baafb0359670b563486bb096abb872234
Noah Tigner <nzt@cs.uoregon.edu>
2021-02-18 12:48:08 -0800

    Quicksort

--- DataStructuresAndAlgorithms/quicksort.py
+++ DataStructuresAndAlgorithms/quicksort.py
@@ -1,35 +1,37 @@
 def quicksort(arr):
-    def _quicksort(arr, low, high):
-        if low < high:
-            partition_index = partition(arr, low, high)
-            _quicksort(arr, low, partition_index-1)
-            _quicksort(arr, partition_index, high)
+    # Average: O(nlogn)
+    # Worst: O(n^2) in the case of bad pivots leading to one long branch
+    # Space: O(1) in-place
+    # Optimization: don't pass arr around internally
 
-    def partition(arr, low, high):
+    def _quicksort(low, high):
+        if low < high:
+            partition_index = partition(low, high)
+            _quicksort(low, partition_index-1)
+            _quicksort(partition_index, high)
+
+    def partition(low, high):
         pivot = arr[(high + low) // 2]
         while low <= high:
-
             # find element on left that should be on right
             while arr[low] < pivot:
                 low += 1
-            
             # find element on right that should be on left
             while arr[high] > pivot:
                 high -= 1
-
             # swap elements, move left and right
             if low <= high:
-                swap(arr, low, high)
+                swap(low, high)
                 low += 1
                 high -= 1
         return low
 
-    def swap(arr, low, high):
+    def swap(low, high):
         temp = arr[low]
         arr[low] = arr[high]
         arr[high] = temp
 
-    _quicksort(arr, 0, len(arr) - 1)
+    _quicksort(0, len(arr) - 1)
     return arr
 
 arr = [1, 2, 3, 4, 5, 44, 55, 22, 1, 55, 7]
